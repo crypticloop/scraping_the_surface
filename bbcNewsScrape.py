a5c52f99f160a83091113c63b8fc4f15be5d35a8
Cryptic Loop <nickstride1@gmail.com>
2018-12-12 22:38:20 +0000

    Added BBC news script

--- bbcNewsScrape.py
+++ bbcNewsScrape.py
@@ -2,22 +2,23 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import time
 
-url = 'https://news.ycombinator.com/news'
+url = 'https://www.bbc.co.uk/news'
 client = uReq(url)
 html = client.read()
 soup = soup(html, "html.parser")
 
-filename = "hackerNewsResults.csv"
+filename = "bbcResults.csv"
 f= open(filename, "w")
 
-headlines = soup.findAll("tr",{"class":"athing"})
+headlines = soup.findAll("h3",{"class":"gs-c-promo-heading__title"})
 headlineTitles = []
 
 for headline in headlines:
-    headlineTitles.append(headline.find("a", {"class":"storylink"}).text)
+    headlineTitles.append(headline.text)
+    print(headline.text)
 
 for headlineTitle in headlineTitles:
     f.write(headlineTitle.replace(",", "|") + "\n")
 
-f.close()
+#f.close()
 client.close()
