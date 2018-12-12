17092a2e31a5124d528a9186e338e766285279e4
Cryptic Loop <nickstride1@gmail.com>
2018-12-12 11:25:18 +0000

    Added timings

--- hackerNewsScrape.py
+++ hackerNewsScrape.py
@@ -1,13 +1,27 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import time
 
+timeBeforeURL = time.time()
 hackerNewsUrl = 'https://news.ycombinator.com/news'
+timeAfterURL = time.time()
 
+print("Time for URL: ", timeAfterURL - timeBeforeURL)
+
+timeBeforeClient = time.time()
 hackerNewsClient = uReq(hackerNewsUrl)
+timeAfterClient = time.time()
 
+print("Time for Client: ", timeAfterClient - timeBeforeClient)
+
+timeBeforeRead = time.time()
 hackerNewsHtml = hackerNewsClient.read()
+timeAfterRead = time.time()
+
+print("Time for Read: ", timeAfterRead - timeBeforeRead)
 
 hackerNewsSoup = soup(hackerNewsHtml, "html.parser")
+
 
 print(hackerNewsSoup.title)
 
