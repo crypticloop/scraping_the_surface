e941a99c41045bb09a72aff81ec926eda4092ad3
Cryptic Loop <nickstride1@gmail.com>
2018-12-23 21:50:50 +0000

    All actions run from single file

--- scrapeFunction.py
+++ scrapeFunction.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import time
 
-def scrape_for_headlines(url, spanTagType, spanClassName, elementTagType, elementClassName):
+def scrape_for_headlines_complicated(url, spanTagType, spanClassName, elementTagType, elementClassName):
     client = uReq(url)
     html = client.read()
     soupPart = soup(html, "html.parser")
@@ -15,6 +15,27 @@ def scrape_for_headlines(url, spanTagType, spanClassName, elementTagType, elemen
 
     for headline in headlines:
         headlineTitles.append(headline.find(elementTagType, {"class":elementClassName}).text)
+
+    for headlineTitle in headlineTitles:
+        # f.write(headlineTitle.replace(",", "|") + "\n")
+        print(headlineTitle.replace(",", "|") + "\n")
+
+    # f.close()
+    client.close()
+
+def scrape_for_headlines_simple(url, spanTagType, firstClassifierType, spanClassName):
+    client = uReq(url)
+    html = client.read()
+    soupPart = soup(html, "html.parser")
+
+    # filename = "hackerNewsResults.csv"
+    # f= open(filename, "w")
+
+    headlines = soupPart.findAll(spanTagType,{firstClassifierType:spanClassName})
+    headlineTitles = []
+
+    for headline in headlines:
+        headlineTitles.append(headline.text)
 
     for headlineTitle in headlineTitles:
         # f.write(headlineTitle.replace(",", "|") + "\n")
--- testFileScrape.py
+++ testFileScrape.py
@@ -1,3 +1,6 @@
 from scrapeFunction import *
 
-scrape_for_headlines('https://news.ycombinator.com/news',"tr","athing","a","storylink")
+scrape_for_headlines_complicated('https://news.ycombinator.com/news',"tr","athing","a","storylink")
+scrape_for_headlines_simple('https://www.bbc.co.uk/news',"h3","class","gs-c-promo-heading__title")
+scrape_for_headlines_simple('https://news.sky.com/',"span","class","sdc-site-tile__headline-text")
+scrape_for_headlines_simple('https://www.telegraph.co.uk/',"h3","class","list-of-entities__item-body-headline")
