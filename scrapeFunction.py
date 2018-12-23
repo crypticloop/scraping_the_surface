df08c8754c29b096d8efd03d07f44c7094fbbeaf
Cryptic Loop <nickstride1@gmail.com>
2018-12-23 21:20:11 +0000

    Added seperate function

---
+++ scrapeFunction.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import time
+
+def scrape_for_headlines(url, spanTagType, spanClassName, elementTagType, elementClassName):
+    client = uReq(url)
+    html = client.read()
+    soupPart = soup(html, "html.parser")
+
+    # filename = "hackerNewsResults.csv"
+    # f= open(filename, "w")
+
+    headlines = soupPart.findAll(spanTagType,{"class":spanClassName})
+    headlineTitles = []
+
+    for headline in headlines:
+        headlineTitles.append(headline.find(elementTagType, {"class":elementClassName}).text)
+
+    for headlineTitle in headlineTitles:
+        # f.write(headlineTitle.replace(",", "|") + "\n")
+        print(headlineTitle.replace(",", "|") + "\n")
+
+    # f.close()
+    client.close()
---
+++ testFileScrape.py
@@ -0,0 +1,3 @@
+from scrapeFunction import *
+
+scrape_for_headlines('https://news.ycombinator.com/news',"tr","athing","a","storylink")
